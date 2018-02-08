87d993786ca18d5ce4ec559f2cf32f61cbe7299d
Kirill Liberis <kyblahan@gmail.com>
2018-02-08 18:38:14 +0300

    Переделка подхода к анализу файлов, есть ошибка с подчетом(анализом файла начиная со 2)

--- FoneNumSort/MainWindow.xaml.cs
+++ FoneNumSort/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,12 +8,13 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using Microsoft.Win32;
+using OpenFileDialog = Microsoft.Win32.OpenFileDialog;
 
 namespace FoneNumSort
 {
@@ -26,6 +28,9 @@ namespace FoneNumSort
         private string pathNew;
         private int counter;
         private string tmpOut;
+        private List<string> dirs;
+        private int CountUniqueNums = 0;
+        private int CountNewNums = 0;
         public MainWindow()
         {
             counter = 0;
@@ -34,10 +39,15 @@ namespace FoneNumSort
 
         private void BtnBase_Click(object sender, RoutedEventArgs e)
         {
-            OpenFileDialog baseData = new OpenFileDialog();
+            FolderBrowserDialog baseData = new FolderBrowserDialog();
+            //OpenFileDialog baseData = new OpenFileDialog();
             baseData.ShowDialog();
-            PathBaseFile.Content = "Путь до базового файла:\n" + baseData.FileName;
-            pathData = baseData.FileName;
+            PathBaseFile.Content = "Путь до базового файла:\n" + baseData.SelectedPath;
+            pathData = baseData.SelectedPath;
+
+            dirs = new List<string>(Directory.GetFiles(pathData));
+            ClearFilies();
+
         }
 
         private void BtnNew_Click(object sender, RoutedEventArgs e)
@@ -48,96 +58,105 @@ namespace FoneNumSort
             pathNew = newData.FileName;
         }
 
+
+        private void ClearFilies()
+        {
+            for (int j = 0; j < dirs.Count; j++)
+            {
+                List<string> ClearingList = new List<string>(File.ReadAllLines(dirs[j], Encoding.Default));
+
+                bool check_1 = false;
+
+                int firstIndex = -1;
+                int lastIndex = -1;
+
+                // очистка от всего лишнего
+                for (var i = 0; i < ClearingList.Count; i++)
+                {
+                    if (ClearingList[i].IndexOf("Дата") >= 0 && check_1 == false)
+                    {
+                        firstIndex = i;
+                        check_1 = true;
+                        break;
+                    }
+                }
+                for (var i = firstIndex; i < ClearingList.Count; i++)
+                {
+                    if (ClearingList[i].IndexOf("ИТОГО:") >= 0)
+                    {
+                        lastIndex = i;
+                        break;
+                    }
+                }
+                ClearingList.RemoveRange(lastIndex, (ClearingList.Count - lastIndex));
+                ClearingList.RemoveRange(0, firstIndex + 1);
+
+                firstIndex = -1;
+                lastIndex = -1;
+                check_1 = false;
+
+                // удаление всего кроме номеров
+                for (var i = 0; i < ClearingList.Count; i++)
+                {
+                    ClearingList[i] = ClearingList[i].Remove(0, 20);
+                    ClearingList[i] = ClearingList[i].Remove(ClearingList[i].IndexOf(' '));
+                }
+
+                ClearingList = ClearingList.Distinct().ToList();
+                //Newlist.RemoveAll(x => x[0] != '7');
+                for (int i = 0; i < ClearingList.Count; i++)
+                {
+                    if (!ClearingList[i].Contains('7'))
+                    {
+                        ClearingList.RemoveAt(i);
+                        i = 0;
+                    }
+                }
+                File.WriteAllLines(dirs[j], ClearingList);
+            }
+        }
+
         private void Start_Click(object sender, RoutedEventArgs e)
         {
-
-            List<string> BaseListTmp = new List<string>(System.IO.File.ReadAllLines(pathData, Encoding.Default));
-            Chain_method BaseList = new Chain_method(100, 3, true);
-
-            foreach (var VARIABLE in BaseListTmp)
+            CountUniqueNums = 0;
+            CountNewNums = 0;
+            List<string> Newlist = new List<string>();
+            for (int j = 1; j < dirs.Count; j++)
             {
-                BaseList.Add(VARIABLE);
-            }
-
-            List<string> Newlist = new List<string>(System.IO.File.ReadAllLines(pathNew, Encoding.Default));
-
-            bool check_1 = false;
-
-            int firstIndex = -1;
-            int lastIndex = -1;
-
-            // очистка от всего лишнего
-            for (var i = 0; i < Newlist.Count; i++)
-            {
-                if (Newlist[i].IndexOf("Дата") >= 0 && check_1 == false)
+                bool check = true;
+                for (int k = 0; k < j; k++)
                 {
-                    firstIndex = i;
-                    check_1 = true;
-                    break;
-                }
-            }
-            for (var i = firstIndex; i < Newlist.Count; i++)
-            {
-                if (Newlist[i].IndexOf("ИТОГО:") >= 0)
-                {
-                    lastIndex = i;
-                    break;
-                }
-            }
-            Newlist.RemoveRange(lastIndex, (Newlist.Count - lastIndex));
-            Newlist.RemoveRange(0, firstIndex + 1);
+                    // создание таблицы из файла K
+                    List<string> BaseListTmp = new List<string>(File.ReadAllLines(dirs[k], Encoding.Default));
+                    Chain_method BaseList = new Chain_method(100, 3, true);
 
-            firstIndex = -1;
-            lastIndex = -1;
-            check_1 = false;
+                    foreach (var VARIABLE in BaseListTmp)
+                    {
+                        BaseList.Add(VARIABLE);
+                    }
+                    /// Конец создания
+                    if (check)
+                    {
+                        Newlist.Clear();
+                        Newlist = new List<string>(File.ReadAllLines(dirs[j], Encoding.Default));
+                        CountUniqueNums = Newlist.Count;
+                    }
 
-            // удаление всего кроме номеров
-            for (var i = 0; i < Newlist.Count; i++)
-            {
-                Newlist[i] = Newlist[i].Remove(0, 20);
-                Newlist[i] = Newlist[i].Remove(Newlist[i].IndexOf(' '));
-            }
+                    for (int i = 0; i < Newlist.Count; i++)
+                    {
+                        if (BaseList.Search(Newlist[i]).Found)
+                        {
+                            Newlist.RemoveAt(i);
+                            i = 0;
+                        }
 
+                    }
 
-
-            Newlist = Newlist.Distinct().ToList();
-            //Newlist.RemoveAll(x => x[0] != '7');
-            for (int i = 0; i < Newlist.Count; i++)
-            {
-                if (!Newlist[i].Contains('7'))
-                {
-                    Newlist.RemoveAt(i);
-                    i = 0;
-                }
-            }
-
-            LblCountAll.Content = "Количество уникальных номеров:  " + Newlist.Count;
-            tmpOut = Newlist.Count.ToString();
-            for (int i = 0; i < Newlist.Count; i++)
-            {
-                if (BaseList.Search(Newlist[i]).Found)
-                {
-                    Newlist.RemoveAt(i);
-                    i = 0;
+                    CountNewNums = Newlist.Count;
                 }
 
+                OutText.Text += CountUniqueNums + " " + CountNewNums + '\n';
             }
-
-            //OutText.Text = string.Join("\n", Newlist.ToArray());
-
-            LblCount.Content = "Количество новых номеров: " + Newlist.Count;
-
-            tmpOut += " " + Newlist.Count.ToString() + '\n';
-            OutText.Text += tmpOut;
-
-            //LblDelta.Content = ;
-            foreach (var VARIABLE in Newlist)
-                BaseList.Add(VARIABLE);
-
-            
-            System.IO.File.WriteAllText(pathData, BaseList.Display());
-            Newlist.Clear();
-            //BaseList.Clear();
         }
 
     }
